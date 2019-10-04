ec02e1b5b726b050eb6f30f50a34356074b69f35
Takács Miklós <takmik92@gmail.com>
2019-10-04 15:50:10 +0200

    nodes and path properly processed, printing out result is still buggy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,5 +10,13 @@ object Main extends App{
 
   val processTriangle = new ProcessTriangle
 
+  val allRoutes = processTriangle.processRoutes(rootNode)
 
+  allRoutes.foreach{
+    item => println(item)
+  }
+
+  val minPath = processTriangle.searchMinPath(allRoutes)
+
+  println(processTriangle.printResult(minPath, rootNode))
 }
--- src/main/scala/ProcessTriangle.scala
+++ src/main/scala/ProcessTriangle.scala
@@ -7,7 +7,7 @@ class ProcessTriangle() {
   def processRoutes(rootNode: Node, route: Route = Route(0, Nil)): List[Route] = {
 
     if(rootNode.leftChild.isEmpty && rootNode.rightChild.isEmpty){
-      val finalRoute = route.copy(sum = route.sum + rootNode.value)
+      val finalRoute = route.copy(sum = route.sum + rootNode.value, dir = route.dir.reverse)
       routes = finalRoute :: routes
     }
     else{
@@ -29,6 +29,24 @@ class ProcessTriangle() {
 
   def printResult(route: Route, rootNode: Node) = {
 
-  }
+    print("Minimum path is: ")
+    var node = rootNode
+
+      for (path <- route.dir) {
+        if (node.leftChild.isEmpty && node.rightChild.isEmpty) {
+          println(" = " + route.sum)
+        }
+        else {
+          if (path == LeftDirection) {
+            print(node.value + " + ")
+            node = node.leftChild.get
+          }
+          if (path == RightDirection) {
+            print(node.value + " + ")
+            node = node.rightChild.get
+          }
+        }
+      }
+    }
 
 }
